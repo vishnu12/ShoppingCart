ca36d4fbfbe962e5ef89d5cd9fcd9aa7c02d3fb4
Vishnu <vishnu007jr@gmail.com>
2020-10-12 19:34:57 +0530

    prodconfig

--- server/server.js
+++ server/server.js
@@ -32,8 +32,21 @@ app.get('/api/config/paypal',(req,res)=>{
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+
 const __dirname=path.resolve() //__dirname is not available for ES module
 app.use('/api/uploads',express.static(path.join(__dirname,'/uploads')))
+
+
+if(process.env.NODE_ENV==='production'){
+   app.use(express.static(path.join(__dirname,'/client/build')))
+   app.use('*',(req,res)=>{
+       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+   })
+}else{
+    app.get('/',(req,res)=>{
+        res.send('API is running')
+    })
+}
 
 
 app.use(notFound)
